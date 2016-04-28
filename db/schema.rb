cd22695cfb2e435e8686a07a520bde16bf61db20
Gerardo Marambio <gerardomarambio@wificlient-10-200-20-242.uc.cl>
2016-04-28 13:16:07 -0300

    rama_admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160428134644) do
 
   # These are extensions that must be enabled in order to support this database
@@ -24,9 +23,9 @@ ActiveRecord::Schema.define(version: 20160428134644) do
     t.string   "salt"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.boolean  "admin"
     t.string   "nombre"
     t.string   "apellido",           default: ""
-    t.boolean  "admin"     ,         defauls: false
   end
 
 end
