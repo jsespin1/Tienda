5564074fb30df6f683804b02ee79eff2f1f34c2b
Javier <jsespin1@uc.cl>
2016-05-12 14:36:53 -0300

    casi nada, par de comentarios

--- app/models/promocion.rb
+++ app/models/promocion.rb
@@ -1,4 +1,14 @@
 class Promocion < ActiveRecord::Base
+	#t.integer  "subtotal"
+    #t.integer  "descuento"
+    #t.string   "nombre"
+    #t.text     "comentario"
+    #t.boolean  "active"
+    #t.string   "imagen"
+
 	has_and_belongs_to_many :products  
 	has_many :order_items 
+
+
+	
 end
