00b331ab140aa93225ebae60ed6a4c1acbe248a4
Javier <jsespin1@uc.cl>
2016-05-17 12:38:57 -0300

    UsuarioNormal

--- app/controllers/catalogo_controller.rb
+++ app/controllers/catalogo_controller.rb
@@ -3,18 +3,25 @@ class CatalogoController < ApplicationController
 
 	def index
 		@items = Catalogo.getRandom
+		@vista = "index"
 	end
 
 	def autos
 		@items = Catalogo.getAutos
+		@vista = "autos"
+		render 'index'
 	end
 
 	def perros
 		@items = Catalogo.getPerros
+		@vista = "perros"
+		render 'index'
 	end
 
 	def promociones
 		@items = Catalogo.getPromociones
+		@vista = "promociones"
+		render 'index'
 	end
 
 
