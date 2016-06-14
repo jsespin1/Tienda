d9c7f761cc5a4daf51941a5cdcbed20e1c9d0bf4
Javier <jsespin1@uc.cl>
2016-06-14 11:00:44 -0400

    se pillo el error de assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,18 +3,19 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-Rails.application.config.assets.precompile += %w( navbar.css )
-Rails.application.config.assets.precompile += %w( users.css )
-Rails.application.config.assets.precompile += %w( sessions.css )
-Rails.application.config.assets.precompile += %w( autos.css )
-Rails.application.config.assets.precompile += %w( editar_catalogo.css )
+#Rails.application.config.assets.precompile += %w( navbar.css )
+#Rails.application.config.assets.precompile += %w( users.css )
+#Rails.application.config.assets.precompile += %w( sessions.css )
+#Rails.application.config.assets.precompile += %w( autos.css )
+#Rails.application.config.assets.precompile += %w( application.css )
+#Rails.application.config.assets.precompile += %w( editar_catalogo.css )
 
-Rails.application.config.assets.precompile += %w( catalogo.css )
-Rails.application.config.assets.precompile += %w( catalogo.js )
-Rails.application.config.assets.precompile += %w( admin.js )
-Rails.application.config.assets.precompile += %w( jquery.js )
-Rails.application.config.assets.precompile += %w( jquery.min.js )
-Rails.application.config.assets.precompile += %w( application.js )
+#Rails.application.config.assets.precompile += %w( catalogo.css )
+#Rails.application.config.assets.precompile += %w( catalogo.js )
+#Rails.application.config.assets.precompile += %w( admin.js )
+#Rails.application.config.assets.precompile += %w( jquery.js )
+#Rails.application.config.assets.precompile += %w( jquery.min.js )
+#Rails.application.config.assets.precompile += %w( application.js )
 
 #Rails.application.config.assets.precompile += %w( admin.js )
 
