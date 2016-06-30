578c037d34a23109a2eaf7fde90abd0e5659dd36
Javier <jsespin1@uc.cl>
2016-06-30 11:17:54 -0400

    hotfix

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,7 +12,7 @@ class Product < ActiveRecord::Base
   validates :descripcion, :presence => true
   validates :imagen, length: { minimum: 0, allow_nil: false, message: "can't be nil" }
 
-  after_create :set_uuid
+  before_create :set_uuid
 
   def self.get_all
     products = Product.all
